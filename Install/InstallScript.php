e0a2a3fe99523e431caf7f4eaa3a6af6f951fa6c
mikl778890@yandex.ru <adminadmin123>
2017-08-26 13:08:31 +0300

    instal commit

---
+++ Install/InstallScript.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Install;
+
+use Composer\Script\Event;
+
+class InstallScript
+{
+
+    static function postInstall(Event $event)
+    {
+        $event->getComposer()->getConfig()->get('vendor-dir');
+        return $event->getComposer()->getConfig()->get('vendor-dir');
+    }
+
+
+}
